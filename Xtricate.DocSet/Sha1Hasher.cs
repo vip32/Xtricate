0816fcfd2c08bb639f07f0906d0e43f5f76f33e7
vip32 <mini32@gmail.com>
2015-11-11 22:56:11 +0100

    v6

---
+++ Xtricate.DocSet/Sha1Hasher.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+
+namespace Xtricate.DocSet
+{
+    public class Sha1Hasher : IHasher
+    {
+        public string Compute(object value)
+        {
+            if (value == null) return null;
+
+            using (var provider = new SHA1Managed())
+            {
+                var hash = provider.ComputeHash(ToBytes(value));
+                return Convert.ToBase64String(hash);
+            }
+        }
+
+        private static byte[] ToBytes<T>(T obj) where T : class
+        {
+            if (obj == null) return null;
+
+            var ms = new MemoryStream();
+            using (var writer = new BsonWriter(ms))
+            {
+                var serializer = new JsonSerializer();
+                serializer.Serialize(writer, obj);
+            }
+            return ms.ToArray();
+        }
+    }
+}
