3d013e90c630fd0ca19d89562b42d23cf593c448
vip32 <mini32@gmail.com>
2015-11-11 22:57:26 +0100

    dd

--- Xtricate.DocSet.Sqlite/Properties/AssemblyInfo.cs
+++ Xtricate.DocSet.Sqlite/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Xtricate.DocSet.Sqlite")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
