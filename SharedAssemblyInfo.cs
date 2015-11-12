250ff9362ed62dbe68825db061a6f0b72b974a69
vip32 <mini32@gmail.com>
2015-11-12 13:35:28 +0100

    v11

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,6 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.1.10")]
-[assembly: AssemblyFileVersion("0.1.10")]
-[assembly: AssemblyInformationalVersion("0.1.10-beta")]
+[assembly: AssemblyVersion("0.1.11")]
+[assembly: AssemblyFileVersion("0.1.11")]
+[assembly: AssemblyInformationalVersion("0.1.11-beta")]
