cc07da1073f8a49e76d79b050b879bc438a0ccae
Vincent van Proosdij <mini32@gmail.com>
2015-11-25 17:10:00 +0100

    RazorGenerator.MsBuild

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.17")]
-[assembly: AssemblyFileVersion("0.1.17")]
-[assembly: AssemblyInformationalVersion("0.1.17-beta")]
+[assembly: AssemblyVersion("0.1.18")]
+[assembly: AssemblyFileVersion("0.1.18")]
+[assembly: AssemblyInformationalVersion("0.1.18-beta")]
