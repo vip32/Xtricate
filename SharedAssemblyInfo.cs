19b95d806ca624928f0fce6ee156a05acfa269db
Vincent van Proosdij <mini32@gmail.com>
2015-11-25 17:37:02 +0100

    SecurityTransparent

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.19")]
-[assembly: AssemblyFileVersion("0.1.19")]
-[assembly: AssemblyInformationalVersion("0.1.19-beta")]
+[assembly: AssemblyVersion("0.1.20")]
+[assembly: AssemblyFileVersion("0.1.20")]
+[assembly: AssemblyInformationalVersion("0.1.20-beta")]
--- Xtricate.Web.Dashboard/Properties/AssemblyInfo.cs
+++ Xtricate.Web.Dashboard/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -21,4 +22,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3fc62e9e-bf9b-4f50-9a8f-5aa74e99b599")]
-[assembly: InternalsVisibleTo("Xtricate.Service.Sample")]
+//[assembly: InternalsVisibleTo("Xtricate.Service.Sample")]
+[assembly: SecurityTransparent]
