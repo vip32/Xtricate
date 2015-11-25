55b44cfb55faf0b2ad7d79430849dca38d346d71
Vincent van Proosdij <mini32@gmail.com>
2015-11-25 15:57:04 +0100

    v16

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.15")]
-[assembly: AssemblyFileVersion("0.1.15")]
-[assembly: AssemblyInformationalVersion("0.1.15-beta")]
+[assembly: AssemblyVersion("0.1.16")]
+[assembly: AssemblyFileVersion("0.1.16")]
+[assembly: AssemblyInformationalVersion("0.1.16-beta")]
