2ae78a7c3e8aae4a0d628d607adb28c1a24342f4
vip32 <mini32@gmail.com>
2015-12-01 17:38:27 +0100

    pack2

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.25")]
-[assembly: AssemblyFileVersion("0.1.25")]
-[assembly: AssemblyInformationalVersion("0.1.25-beta")]
+[assembly: AssemblyVersion("0.1.26")]
+[assembly: AssemblyFileVersion("0.1.26")]
+[assembly: AssemblyInformationalVersion("0.1.26-beta")]
