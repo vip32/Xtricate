9ec6ed8735143027895c4616f6a42ea52f4ebfe9
vip32 <mini32@gmail.com>
2015-12-02 16:40:58 +0100

    sqlite

---
+++ Xtricate.DocSet.Sqlite/SqliteConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Common;
+using System.Data.Entity.Infrastructure;
+using System.Data.SQLite;
+
+namespace Xtricate.DocSet.Sqlite
+{
+    public class SqliteConnectionFactory : IDbConnectionFactory
+    {
+        /// <summary>
+        /// Creates the connection.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        /// <returns></returns>
+        public DbConnection CreateConnection(string connectionString)
+        {
+            return new SQLiteConnection(connectionString);
+        }
+    }
+}
