a1444a882de8c3108bc4c472e4fe955f6c9e51f7
Vincent van Proosdij <mini32@gmail.com>
2016-02-03 17:14:18 +0100

    humanizer

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.51")]
-[assembly: AssemblyFileVersion("0.1.51")]
-[assembly: AssemblyInformationalVersion("0.1.51")]
+[assembly: AssemblyVersion("0.1.52")]
+[assembly: AssemblyFileVersion("0.1.52")]
+[assembly: AssemblyInformationalVersion("0.1.52")]
