45bf92636939735e28f668ea1e3150a8724941ba
vip32 <mini32@gmail.com>
2016-02-15 23:05:50 +0100

    16

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.15")]
-[assembly: AssemblyFileVersion("1.0.15")]
-[assembly: AssemblyInformationalVersion("1.0.15")]
+[assembly: AssemblyVersion("1.0.16")]
+[assembly: AssemblyFileVersion("1.0.16")]
+[assembly: AssemblyInformationalVersion("1.0.16")]
