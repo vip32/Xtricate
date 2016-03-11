e0c99a72491ee40ff216584d2c703e00c5be3dbc
vip32 <mini32@gmail.com>
2016-03-11 13:28:08 +0100

    fff

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.33")]
-[assembly: AssemblyFileVersion("1.0.33")]
-[assembly: AssemblyInformationalVersion("1.0.33")]
+[assembly: AssemblyVersion("1.0.34")]
+[assembly: AssemblyFileVersion("1.0.34")]
+[assembly: AssemblyInformationalVersion("1.0.34")]
