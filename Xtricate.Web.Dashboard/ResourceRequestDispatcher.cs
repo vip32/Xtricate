eb5294afb13756a6e7865164749d00b917b6c87a
vip32 <mini32@gmail.com>
2016-03-16 16:46:54 +0100

    add

---
+++ Xtricate.Web.Dashboard/ResourceRequestDispatcher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+
+namespace Xtricate.Web.Dashboard
+{
+    public class ResourceRequestDispatcher : IRequestDispatcher
+    {
+        private readonly Func<Match, ResourceDispatcher> _resourceFunc;
+
+        public ResourceRequestDispatcher(Func<Match, ResourceDispatcher> resourceFunc)
+        {
+            _resourceFunc = resourceFunc;
+        }
+
+        public Task Dispatch(RequestDispatcherContext context)
+        {
+            // execute the template
+            var resource = _resourceFunc(context.UriMatch);
+            return resource.Dispatch(context);
+        }
+    }
+}
