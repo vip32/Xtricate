bf96c5c9437aa47e0f9ae44294cee2befee26595
FIXED-TERM Proosdij von Vincent (CI/WBE3-EU) <fixed-term.Vincent.vanProosdij2@de.bosch.com>
2016-04-30 00:44:22 +0200

    VAL+DAT

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.50")]
-[assembly: AssemblyFileVersion("1.0.50")]
-[assembly: AssemblyInformationalVersion("1.0.50")]
+[assembly: AssemblyVersion("1.0.51")]
+[assembly: AssemblyFileVersion("1.0.51")]
+[assembly: AssemblyInformationalVersion("1.0.51")]
