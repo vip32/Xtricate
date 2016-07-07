345853233ce0d9c32a869d56df6de9ec94839189
vip32 <mini32@gmail.com>
2016-07-07 12:31:26 +0200

    core test

---
+++ src/Xtricate.Core.Test/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Xtricate.Core.Test
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            http://www.michael-whelan.net/porting-dotnet-framework-library-to-dotnet-core/
+        }
+    }
+}
---
+++ src/Xtricate.Core.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Xtricate.Core.Test")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("27dcfcb4-a4c9-44b9-96a6-e12186be443d")]
